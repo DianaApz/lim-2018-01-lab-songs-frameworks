913221f45882a69edfdf31a042dce85f70f8a873
DianaApz <kari_dia_12@hotmail.com>
2018-10-09 16:10:56 -0500

    Updates

---
+++ precache-manifest.2a6cfb66e618e1d24008783aac34964b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "4bbb13f5edb8b3b932a3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.4bbb13f5.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "4bbb13f5edb8b3b932a3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.669e8b46.chunk.css"
+  },
+  {
+    "revision": "c3d7f93906db9ebdc6b04386ee130b26",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
